40b64c54a7098914373d3e8a58b237ca1db0e420
MONITORING HUB <61230711+serignedia@users.noreply.github.com>
2023-01-29 21:58:27 +0100

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -188,4 +188,21 @@ const config = {
     >GitProfile</a> and ❤️`,
 };
 
+import GitProfile from '@arifszn/gitprofile';
+import '@arifszn/gitprofile/dist/style.css';
+
+function App() {
+  return (
+    <GitProfile
+      config={{
+        github: {
+          username: 'arifszn',
+        },
+      }}
+    />
+  );
+}
+
+export default App;
+
 export default config;
